3cc75862173b4ff5733fae38777356d07766a31c
Muhammad Saleh Saqib <salehsaqib@gmail.com>
2024-08-08 00:32:20 +0500

    new item added with its method

--- class_assert.py
+++ class_assert.py
@@ -12,14 +12,20 @@ class Item:
     def calculate_total_price(self):
         return self.price * self.quantity
 
-item1 = Item("phone", 30000, 1)
-item2 = Item("Laptop", 100000, 3)
+item1 = Item("phone", 30000, 11)
+item2 = Item("Laptop", 100000, 23)
 item3 = Item("mobile", 2000, 13)
 
+item4 = Item("chair", 30, 1)
+item5 = Item("table", 100, 3)
+item6 = Item("fan", 20, 13)
 
-print(item1.calculate_total_price())
 print(item2.calculate_total_price())
 print(item3.calculate_total_price())
+
+print(item4.calculate_total_price())
+print(item5.calculate_total_price())
+print(item6.calculate_total_price())
 
 
 
